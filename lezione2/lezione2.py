214d553f626b938be5892febe54c7e297bc899c3
Osamak7 <hliwaoussama7@gmail.com>
2024-04-18 13:40:55 +0200

    Svolto esercizio 3.1  - 3.2  - 3.3 - 3.4 - 3.5

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -39,3 +39,44 @@ to a variable called filename. Then use the removesuffix() method to display the
 nome_file:str="python_notes.txt"
 nome_file=nome_file.removesuffix(".txt")
 print(nome_file)
+
+"""3-1. Names: Store the names of a few of your friends in a list called names. Print each person’s name by accessing
+ each element in the list, one at a time."""
+ 
+lista_nomi:list=["Berlusconi","Paperon de Paperoni","Totti"]
+for i in lista_nomi:
+    print(i)
+
+"""3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person’s name,
+ print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name."""
+
+for i in lista_nomi:
+    print(f"{i} ciao come stai ?")
+
+""""3-3. Your Own List: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples.
+ Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”"""
+
+auto:list=["vorrei possedere una lamborghini","mi piacerebbe possedere una ferrari ", "mi stuzzicherebbe avere una pagani"]
+for i in auto:
+    print(i)
+
+"""3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes 
+at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner."""
+
+lista_invitati:list=["Bill Gates","Warren Buffet","Cristino Ronaldo"]
+for n in lista_invitati:
+    print(f"Ciao {n} vorrei invitarti per avere un confronto e dei suggerimenti riguardo al futuro")
+
+"""3-5. Changing Guest List: You just heard that one of your guests can’t make the dinner, 
+so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+• Start with your program from Exercise 3-4. Add a print() call at the end of your program, stating the name of the guest who can’t make it.
+• Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+• Print a second set of invitat"""
+
+non_puo_venire:str=lista_invitati.pop(1)
+print(f"{non_puo_venire} ha appena comunicato che non ci sarà")
+nuovo_invitato:str="Pogba"
+lista_invitati.append(nuovo_invitato)
+print(lista_invitati)
+for n in lista_invitati:
+    print(f"Ciao {n} vorrei invitarti per avere un confronto e dei suggerimenti riguardo al futuro")
