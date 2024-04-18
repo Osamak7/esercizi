b80a3e5fac4adcf3f5c2ca3d5c175e64e19efff1
Osamak7 <hliwaoussama7@gmail.com>
2024-04-18 12:25:12 +0200

    Svolto esercizio 2.3 - 2.4

---
+++ lezione2/lezione2.py
@@ -0,0 +1,20 @@
+#Oussama hliwa
+#18/04/2024
+
+"""2-3. Personal Message: Use a variable to represent a person’s name,
+and print a message to that person. Your message should
+be simple, such as, “Hello Eric, would you like to learn some Python today?”"""
+
+nome:str="Piero"
+print(f"ciao {nome}, ti va di imparare python insieme oggi ? ")
+
+"""2-4. Name Cases: Use a variable to represent a person’s name, 
+and then print that person’s name in lowercase, uppercase, and title case."""
+
+nome_utente:str="Luigi"
+nome_upper:str=nome_utente.upper()
+print(nome_upper)
+nome_lower:str=nome_utente.lower()
+print(nome_lower)
+nome_title:str=nome_utente.title()
+print(nome_title)
