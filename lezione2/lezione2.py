ec190771cc52619c1a32b181a15ef3e16ba0403d
Osamak7 <hliwaoussama7@gmail.com>
2024-04-18 13:57:29 +0200

    Svolto esercizio 3.6

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -80,3 +80,20 @@ lista_invitati.append(nuovo_invitato)
 print(lista_invitati)
 for n in lista_invitati:
     print(f"Ciao {n} vorrei invitarti per avere un confronto e dei suggerimenti riguardo al futuro")
+
+
+"""
+3-6. More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+• Start with your program from Exercise 3-4 or 3-5. Add a print() call to the end of your program, informing people that you found a bigger table.
+• Use insert() to add one new guest to the beginning of your list.
+• Use insert() to add one new guest to the middle of your list.
+• Use append() to add one new guest to the end of your list.
+• Print a new set of invitation messages, one for each person in your list."""
+
+print("ho appena trovato un tavolo più grande per invitare nuovi invitati ")
+lista_invitati.insert(1,"Matuidi")
+lista_invitati.insert(2,"Zidan")
+lista_invitati.append("M.Jordan")
+print(lista_invitati)
+for n in lista_invitati:
+    print(f"Ciao {n} vorrei invitarti per avere un confronto e dei suggerimenti riguardo al futuro")
