682573d8e183334bd861d1387c3b8e2ace8d3f8d
Osamak7 <hliwaoussama7@gmail.com>
2024-04-20 20:24:46 +0200

    Svolto esercizio 5 lezione 4 e diviso gli esercizi

--- lezione4/lezione4.py
+++ lezione4/lezione4.py
@@ -3,6 +3,8 @@ def subtract(a:int,b:int):
     return res 
 print(subtract(int(input()),int(input())))
 
+
+#Exercise 1
 def check_value(a:int):
     if a == 5:
         
@@ -13,6 +15,7 @@ def check_value(a:int):
         return "il numero è minore"
 print(check_value(int(input("inserisci un numero intero "))))
 
+#Excercise 2
 def check_lenght(frase:str):
     a=[]
     for l in frase:
@@ -25,11 +28,13 @@ def check_lenght(frase:str):
         return "è minore"
 print(check_lenght(input("inserisci la frase: ")))
 
+#Excercise 3
 def print_number(elementi:list):
      for e in elementi:
           print(e)
 print_number([1,2,3,4,5,6,7,8,9])
 
+#Excercise 4
 def check_each(numbers:list):
      for n in numbers:
         if n == 5:
@@ -39,3 +44,17 @@ def check_each(numbers:list):
         else:
              print(f"{n} è minore di 5")
 check_each([1,2,3,4,5,6,7,8,9])
+
+#Excercise 5
+def add_one(numero:int):
+     numero+=1
+     return numero
+print(add_one(5))
+
+def add_one_to_list(lista:list):
+    new_list:list=[]
+    for v in lista:
+        new_list.append(add_one(v))
+    print(new_list)
+    
+add_one_to_list([1,2,3,4,5,6,7,8,9])
