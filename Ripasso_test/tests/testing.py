b8835cb11f3edec29493fe4925f0cf4b97aeb12e
Osamak7 <hliwaoussama7@gmail.com>
2024-06-05 18:25:21 +0200

    aggiunta cartella Ripasso_test

---
+++ Ripasso_test/tests/testing.py
@@ -0,0 +1,13 @@
+import unittest
+from Ripasso.ripasso import Calc
+
+class TestCalculations(unittest.TestCase):
+
+    def setUp(self):
+        self.calculation = Calc(8,2)
+
+    def test_sum(self):
+        self.assertEqual(self.calculation.get_sum(), 10, "The sum is wrong. ")
+
+if __name__ == "__main__":
+    unittest.main()
